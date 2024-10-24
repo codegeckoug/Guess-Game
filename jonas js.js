7ea5b3a48ac73deaa90fcf4da32d64c4b8cd4ba5
codegeckoug <154829257+codegeckoug@users.noreply.github.com>
2024-10-24 14:38:57 +0100

    commit changes

--- jonas js.js
+++ jonas js.js
@@ -1,7 +1,7 @@
 "use strict";
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let playGame;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -15,8 +15,25 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+    // guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "Too High" : "Too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
 
-    //when guess is too high
+/* //when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too High";
@@ -38,10 +55,11 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
-});
+});*/
 
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
+
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
